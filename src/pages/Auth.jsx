06e7f6d8ee27dca28a6a2f2abb65a0243273d71c
impritamk <im.pritamk@gmail.com>
2026-05-27 16:33:57 +0530

    Switched to Create React App config and REACT_APP variables

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 // Replace this string with your REAL Render URL (no slash at the end)
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 
 const Auth = () => {
